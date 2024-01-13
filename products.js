fb372aa17356827c6c286a3e1b97c0edfe4bbdde
SBala1398 <sombala1398@gmail.com>
2024-01-12 23:16:22 -0800

    update the site with js

--- products.js
+++ products.js
@@ -9,3 +9,24 @@ menuicon.addEventListener("click",function(){
 closenav.addEventListener("click",function(){
     sidenav.style.right="-50%"
 })
+// product Search Action
+var productContainer = document.getElementById("product-container")
+var search = document.getElementById("search")
+var productlist = productContainer.querySelectorAll("div")
+
+ search.addEventListener("keyup",function(){
+    var enterdValude = event.target.value.toUpperCase()
+
+    for (count = 0; count<productlist.length; count=count+1)
+    {
+        var productname = productlist[count].querySelector("h2").textContent
+
+        if (productname.toUpperCase().indexOf(enterdValude)<0)
+        {
+            productlist[count].style.display="none"
+        }
+        else{
+            productlist[count].style.display="block"
+        }
+    }
+ })
